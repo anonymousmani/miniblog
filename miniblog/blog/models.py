9df6a83c6e6514c9bebbfe0b275c38a5ffa8a23c
Brijesh kumar <59088458+anonymousmani@users.noreply.github.com>
2023-10-31 23:53:56 +0530

    Update models.py

--- miniblog/blog/models.py
+++ miniblog/blog/models.py
@@ -2,5 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
- title = models.CharField(max_length=150)
- desc = models.TextField()
+  title = models.CharField(max_length=150)
+  desc = models.TextField()
+
+class contact(models.Model):
+  name = models.CharField(max_length=30)
+  email = models.CharField(max_length=40)
+  address = models.CharField(max_length=30)
+  message = models.CharField(max_length=150)
