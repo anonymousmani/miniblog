05033a44f400bba5098359d7b6e3215cb4a91b03
Brijesh kumar <59088458+anonymousmani@users.noreply.github.com>
2023-10-31 23:53:28 +0530

    Update form.py

--- miniblog/blog/form.py
+++ miniblog/blog/form.py
@@ -1,23 +1,34 @@
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField
+from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
+from .models import Post, contact
 
 class user_form(UserCreationForm):
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'from-control'}))
-    password2 = forms.CharField(label='Confirm Password',  widget=forms.PasswordInput(attrs={'class': 'from-control'}))
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label='Confirm Password',  widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email']
         labels = {'first_name': 'First Name', 'last_name': 'Last Name',
                   'email': 'Email'
                   }
-        widget = {'username': forms.TextInput(attrs={'class': 'from-control'}),
-                  'first-name': forms.TextInput(attrs={'class': 'from-control'}),
-                  'last_name': forms.TextInput(attrs={'class': 'from-control'}),
-                  'email': forms.EmailInput(attrs={'class': 'from-control'})
-                  
-                  
-                  }
+        widgets = {
+                  'username': forms.TextInput(attrs={'class': 'form-control'}),
+                  'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+                  'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+                  'email': forms.EmailInput(attrs={'class': 'form-control'}),
+		  }
 
-#class login_form(AuthenticationForm):
-#    pass
+class blog_form(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'desc']
+        labels = {'title': 'Title', 'desc': 'Description'}
+        widget = {
+                  'title': forms.TextInput(attrs={'class': 'form-control'}),
+                  'desc': forms.Textarea(attrs={'class': 'form-control'}),
+                }
+
+
+class contact_form(forms.ModelForm):
+    pass      
