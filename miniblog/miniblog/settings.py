799f0b8802a3c3f5fa300567b04fe1b7bebcac21
Brijesh kumar <59088458+anonymousmani@users.noreply.github.com>
2023-11-16 11:45:27 +0530

    Update settings.py

--- miniblog/miniblog/settings.py
+++ miniblog/miniblog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1sfpp6=ww5#32^ac6nccf6vnwt(vkm2xi3+j0ot*rk!w!qgf&q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
